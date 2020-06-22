653847a3b9ec82f8a44291e7e4951e3c70580be3
Jinpeng Zhu <dpthinker@qq.com>
2020-06-23 07:41:06 +0800

    add plus-one

---
+++ Week_01/66.plus-one.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            digits[i]++;
+            digits[i] %= 10;
+            if (digits[i]) return digits;
+        }
+        vector<int> res(digits.size() + 1, 0);
+        res[0] = 1;
+        return res;
+    }
+};
